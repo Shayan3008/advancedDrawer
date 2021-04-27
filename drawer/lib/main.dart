b244a1374fcf3dac45905c85095b5effe0213d6c
shayan ali <shayanjawed4@gmail.com>
2021-04-27 22:56:41 +0500

    drag drawer

--- drawer/lib/main.dart
+++ drawer/lib/main.dart
@@ -11,7 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   late AnimationController controller;
-
+  late bool drag;
   @override
   void initState() {
     super.initState();
@@ -39,26 +39,55 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 alignment: Alignment.centerLeft,
                 child: Column(
                   children: [
-                    for (var i = 0; i < 5; i++) Text("$i",style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),),
+                    for (var i = 0; i < 5; i++)
+                      Text(
+                        "$i",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                   ],
                 ),
               ),
             ),
-            Transform(
-              transform: Matrix4.identity()
-                ..translate(150 * controller.value)
-                ..scale(1 - (controller.value * 0.3)),
-              alignment: Alignment.centerRight,
-              child: Container(
-                color: Colors.yellow,
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: GestureDetector(
-                    onTap: toggle,
-                    child: Icon(Icons.dehaze),
+            GestureDetector(
+              onHorizontalDragStart: (DragStartDetails dragStartDetails) {
+                bool choice1 = controller.isDismissed &&
+                    dragStartDetails.globalPosition.dx > 0;
+                bool choice2 = controller.isDismissed &&
+                    dragStartDetails.globalPosition.dx < 0;
+                drag = choice1 || choice2;
+                print(drag);
+              },
+              onHorizontalDragUpdate: (details) {
+                if (drag) {
+                  var delta = details.primaryDelta! / 0.5;
+                  controller.value += delta;
+                  print(delta);
+                  print(controller.value);
+                  toggle();
+                }
+              },
+              onHorizontalDragCancel: () {
+                if (controller.isDismissed || controller.isCompleted) return;
+                if (controller.value == 1 || controller.value == 0)
+                  controller.reset();
+                
+              },
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..translate(150 * controller.value)
+                  ..scale(1 - (controller.value * 0.3)),
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.yellow,
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: GestureDetector(
+                      onTap: toggle,
+                      child: Icon(Icons.dehaze),
+                    ),
                   ),
                 ),
               ),
